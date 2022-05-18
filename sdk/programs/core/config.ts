f2235367f382a3eb04025adef8d3a6a88acf884f
checho1989 <cheverdolaga@hotmail.com>
2022-05-18 13:14:04 -0400

    Update programs ids

--- sdk/programs/core/config.ts
+++ sdk/programs/core/config.ts
@@ -3,10 +3,10 @@ export const config = {
     arweaveWallet: 'HvwC9QSAzvGXhhVrgPmauVwFWcYZhne3hVot9EbHuFTm',
     programs: {
       // Metaplex
-      auction: '2bhX9H5dAYaNqCHWVuJphto8eAwAxSQVRzvT5KJDs6eW',
+      auction: 'yauNkf2KVyLp9YBQb4mNeiwFCCWu1Vei9Tx3EsgCESG',
       metadata: 'metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s',
-      metaplex: 'Fr6ufqFxV3AuYEPYDvRjR2ghaCg6RyzvU3k9pC5z6x94',
-      vault: '4CJLCFj8dhHmAC3TJizaN2BqkzW6hDeviZwH9qsSBjwH',
+      metaplex: 'yp1ZrQ2ghLMDNdaGdYLiwi8QRFyws2tAHNa7JG2VuTq',
+      vault: 'yvaUJxtAiuphyL7JiVMMdY7uTJe1ekb4LmHXtBv5SFd',
       packs: 'BNRmGgciUJuyznkYHnmitA9an1BcDDiU9JmjEQwvBYVR',
       // External
       memo: 'MemoSq4gqABAXKb96qnH8TysNcWxMyWCqXgDLGmfcHr',
